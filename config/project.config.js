2c689cd417bd5789d4fd1fb0c317cb5f8dd3d64d
Vlad Ardelean <vlad.ardelean@trustyou.net>
2017-03-08 00:54:09 +0200

    updated javascript port setting

--- config/project.config.js
+++ config/project.config.js
@@ -25,7 +25,7 @@ const config = {
   // Server Configuration
   // ----------------------------------
   server_host : ip.address(), // use string 'localhost' to prevent exposure on local network
-  server_port : process.env.PORT || 3027,
+  server_port : process.env.PORT || 3214,
 
   // ----------------------------------
   // Compiler Configuration
